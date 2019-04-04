93879045d39e30aa69c5580a3b8e0ab7a229e948
minlucyzhao <minlucyzhao@gmail.com>
2019-04-04 18:05:46 +0000

    Prints the index of where Waldo is found

---
+++ callback-arg.js
@@ -0,0 +1,16 @@
+// Log the index of the array where Waldo is found
+function findWaldo(arr, found) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === "Waldo") {
+        found(i);   // execute callback
+      }
+    }
+  }
+  
+  function actionWhenFound(waldoLocation) {
+    console.log("Found him!");
+    console.log(waldoLocation);
+  }
+  
+  findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+  
